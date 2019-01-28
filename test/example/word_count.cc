0e5bdea0a992b8efd028859930c62c96dad4f4ec
Junhao Li <42052294+junhao12131@users.noreply.github.com>
2019-01-28 00:20:03 -0500

    Add examples to test (#4)

---
+++ test/example/word_count.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <gtest/gtest.h>
+
+#include "../../blaze.h"
+
+TEST(ExampleTest, WordCount) { 
+  auto lines = blaze::util::load_file("test/example/word_count_example.txt");
+  const auto& mapper = [&](const size_t, const std::string& line, const auto& emit) {
+    std::stringstream ss(line);
+    std::string word;
+    while (getline(ss, word, ' ')) emit(word, 1);
+  };
+  blaze::DistHashMap<std::string, size_t> words;
+  blaze::mapreduce<std::string, std::string, size_t>(lines, mapper, "sum", words);
+  std::cout << words.get_n_keys() << std::endl;
+}
