360c22433b6cbb1ac7d926f5f7876fe3e590c5f0
Junhao Li <junhao12131@gmail.com>
2019-01-28 00:16:41 -0500

    .

---
+++ test/example/pi_estimate.cc
@@ -0,0 +1,25 @@
+#include <gtest/gtest.h>
+
+#include <chrono>
+
+#include "../../blaze.h"
+
+double thread_safe_rand() {
+  static thread_local std::mt19937 generator(
+      std::chrono::system_clock::now().time_since_epoch().count());
+  std::uniform_real_distribution<double> distribution;
+  return distribution(generator);
+}
+
+TEST(ExampleTest, PiEstimate) {
+  const size_t N_SAMPLES = 1000000;
+  blaze::DistRange<size_t> samples(0, N_SAMPLES);
+  const auto& mapper = [&](const size_t, const auto& emit) {
+    double x = thread_safe_rand();
+    double y = thread_safe_rand();
+    if (x * x + y * y < 1) emit(0, 1);
+  };
+  std::vector<size_t> count(1);
+  blaze::mapreduce<size_t, size_t>(samples, mapper, "sum", count);
+  std::cout << 4.0 * count[0] / N_SAMPLES << std::endl;
+}
